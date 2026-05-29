9246e6142d39ba81d4bbe009c8d1e7be362c442f
ihsanaytar10-boop <ihsanaytar10@gmail.com>
2026-05-29 02:18:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
-const { REST, Routes } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  SlashCommandBuilder,
+  REST,
+  Routes
+} = require("discord.js");
 
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+const symbols = ["🍒", "🍋", "⭐", "7️⃣"];
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("slot")
+    .setDescription("🎰 Slot")
+].map(c => c.toJSON());
+
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 (async () => {
-  try {
-    await rest.put(
-      Routes.applicationCommands(CLIENT_ID),
-      { body: [] }
-    );
+  await rest.put(
+    Routes.applicationCommands(CLIENT_ID),
+    { body: commands }
+  );
 
-    console.log("Alle Commands gelöscht");
-  } catch (err) {
-    console.error(err);
-  }
+  console.log("Commands geladen");
 })();
+
+client.once("ready", () => {
+  console.log("Bot online");
+});
+
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "slot") {
+
+    const r = () =>
+      symbols[Math.floor(Math.random() * symbols.length)];
+
+    const row1 = `${r()} ${r()} ${r()}`;
+    const row2 = `${r()} ${r()} ${r()}`;
+    const row3 = `${r()} ${r()} ${r()}`;
+
+    interaction.reply({
+      content:
+"```txt\n" +
+row1 + "\n" +
+row2 + "\n" +
+row3 +
+"\n```"
+    });
+  }
+});
+
+client.login(TOKEN);
